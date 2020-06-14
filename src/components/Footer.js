f1201fed5120cc38ce284cd82f987228c8f9f451
Freely Francis <freelyfranciz@gmail.com>
2020-06-14 18:23:22 -0400

    Added text field

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,25 +1,12 @@
 import React, { Component } from 'react';
 
 class Footer extends Component {
-    constructor() {
-        super();
-        this.state = {
-            isLoggedIn: "false"
-        }
-        this.handleClick = this.handleClick.bind(this);
-    }
-
-    handleClick() {
-        this.setState((prevState) => {
-            return { isLoggedIn: !prevState.isLoggedIn }
-        });
-    }
+  
 
     render() {
         return (
             <footer>
-                <p>{this.state.isLoggedIn ? "You are logged in" : "You are logged out"}</p>
-                <button onClick={this.handleClick}>{this.state.isLoggedIn ? "log in" : "log out"}</button>
+              
             </footer>
         );
     }
--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -6,12 +6,15 @@ class MainContent extends Component {
   constructor() {
     super();
     this.state = {
-      todos: todosData
+      todos: todosData,
+      value: ''
     }
     this.handleChange = this.handleChange.bind(this);
+    this.updateItem = this.updateItem.bind(this);
+    this.handleAddItem = this.handleAddItem.bind(this);
   }
 
-  handleChange(id, c) {
+  handleChange(id) {
     this.setState(prevState => {
       const updatedTodos = prevState.todos.map(todo => {
         if (todo.id === id) {
@@ -25,11 +28,36 @@ class MainContent extends Component {
     })
   }
 
+  updateItem(evt) {
+    this.setState({
+        value: evt.target.value
+      });
+  }
+
+  handleAddItem() {
+  
+   this.setState((prevState) => {
+    const newTodos = [...prevState.todos, {
+      id: Object.keys(prevState.todos).length + 1,
+      text: this.state.value,
+      completed: false
+    }];
+
+    
+    console.log(newTodos);
+    return {todos : newTodos};
+   });
+
+
+  }
+
   render() {
     const todoitems = this.state.todos.map((item) =>
       <ToDoItem key={item.id} item={item} handleChange={this.handleChange} />)
     return (
       <div>
+        <input type="text" placeholder="Enter item" onChange={this.updateItem} />        
+        <button  onClick={this.handleAddItem}>Add</button>
         {todoitems}
       </div>
     );
